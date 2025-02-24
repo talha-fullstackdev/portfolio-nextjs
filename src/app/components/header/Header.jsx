d4afb62672c25a28e938e9d7cbf8d8e48b83ccf0
talha-fullstackdev <talhafullstack786@gmail.com>
2025-02-24 12:21:16 -0800

    add projects folders

---
+++ src/app/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from './Logo'
+import Navbar from './Navbar'
+const Header = () => {
+  return (
+    <div>
+        <Logo/>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Header
