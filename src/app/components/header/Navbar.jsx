8b03abb5ea18a7841117b96d735c6822fb490f3c
talha-fullstackdev <talhafullstack786@gmail.com>
2025-02-24 14:59:24 -0800

    made hero section

--- src/app/components/header/Navbar.jsx
+++ src/app/components/header/Navbar.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import { IoSunnyOutline } from "react-icons/io5";
 
 const Navbar = () => {
+  const linkStyles="hover:text-blue-400"
   return (
     <>
       <ul className="flex flex-col gap-2 text-xl mt-3 md:flex-row md:justify-center md:items-center md:gap-10 md:mt-0 md:py-2 flex-wrap">
-        <a href="">About</a>
-        <a href="">Skills</a>
-        <a href="">Experience</a>
-        <a href="">Projects</a>
-        <a href="">Contact</a>
+        <a className={linkStyles} href="">About</a>
+        <a className={linkStyles} href="">Skills</a>
+        <a className={linkStyles} href="">Experience</a>
+        <a className={linkStyles} href="">Projects</a>
+        <a className={linkStyles} href="">Contact</a>
         <a
           href=""
           className="relative text-blue-400 border p-1 px-2 rounded-md border-blue-400 overflow-hidden group"
@@ -17,7 +18,7 @@ const Navbar = () => {
           <span className="absolute inset-0 border-2 border-blue-400 rounded-md animate-border-glow"></span>
           <span className="relative z-10">Resume</span>
         </a>
-        <button className="text-3xl ">
+        <button className="text-3xl hover:text-orange-300">
           <IoSunnyOutline />
         </button>
       </ul>
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero";
 export default function Home() {
   return (
     <>
      <Header/>
+     <Hero/>
     </>
   );
 }
